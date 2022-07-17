5c3a0868e809afaf17b3a8bf7795348fbca1c8f5
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-07-18 01:13:06 +0700

    Update info1.js

--- message/info1.js
+++ message/info1.js
@@ -23,22 +23,11 @@ return`
 *▸* Voice : Nina kawai
 
 *▸* SCRIPT BY
-   Decode Denpa
-   X-Dev Team
-   Yogi PW
-   Hexagon
-   Dttaz
+   *Lintang*
 
 *▸* FEATURE  BY  
-   Decode Denpa
-   Fernazer
-   X-Dev Team
-   Resta Gamteng
-   Zeeone Ofc
-   X-Dev Team
-   Yudha perdana
-   Xchilds
-   Dika Ardnt
+   *Sad Botz*
+
 `
 }
 
